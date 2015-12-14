2e2f8f64453da45d783b09eb375e053117f0d13c
Denis Stepanov <denstepa@gmail.com>
2015-12-14 11:31:13 +0100

    add option to trust time and not override it on server

--- lib/ahoy/tracker.rb
+++ lib/ahoy/tracker.rb
@@ -13,7 +13,7 @@ module Ahoy
       unless exclude?
         options = options.dup
 
-        options[:time] = trusted_time(options[:time])
+        options[:time] = options[:time] && options[:trust_time] ? options[:time] : trusted_time(options[:time])
         options[:id] = ensure_uuid(options[:id] || generate_id)
 
         @store.track_event(name, properties, options)
